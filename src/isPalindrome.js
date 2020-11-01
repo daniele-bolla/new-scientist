7e5c1c9ae22a36e881e13dd9a2be3b5afe432ada
bolladaniele <bolladaniele@gmail.com>
2020-11-01 11:16:25 +0000

    IsPalindrome

---
+++ src/isPalindrome.js
@@ -0,0 +1,14 @@
+
+const clean = (str) => str.toLowerCase().replace(/[\W_]/g, "");
+
+export const isPalindrome = (str) => {
+  const cleanStr = clean(str);
+  const len = cleanStr.length;
+  for (var i = 0; i < len / 2; i++) {
+    console.log(cleanStr[i], cleanStr[len - 1 - i]);
+    if (cleanStr[i] !== cleanStr[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+};
---
+++ src/isPalindrome.spec.js
@@ -0,0 +1,9 @@
+import { isPalindrome } from "./isPalindrome"
+describe('isPalindrome', () => {
+  it('checks if a word is a palindrome', () => {
+    expect(isPalindrome('kayak')).toBeTruthy();
+  });
+  it('checks if a phrase is a palindrome', () => {
+    expect(isPalindrome('A man, a plan, a canal. Panama')).toBeTruthy();
+  });
+});
